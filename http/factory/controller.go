6166b0df22fff373770f0857587becbc4db88e8d
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-25 12:47:16 -0400

    refactor: create Service, Validator, Controller and Module structs (part IV)

--- http/factory/controller.go
+++ http/factory/controller.go
@@ -1,14 +1,43 @@
 package factory
 
 import (
+	"github.com/goccy/go-json"
 	gonethttproute "github.com/ralvarezdev/go-net/http/route"
 )
 
 type (
 	// ControllerWrapper is the interface for the route controller
 	ControllerWrapper interface {
+		Create(baseRouter gonethttproute.RouterWrapper, path string) error
 		RegisterRoutes()
 		RegisterGroups()
 		gonethttproute.RouterWrapper
 	}
+
+	// Controller is the struct for the route controller
+	Controller struct {
+		gonethttproute.RouterWrapper
+	}
+
+	// Create creates the controller
+	func (c *Controller) Create(baseRouter gonethttproute.RouterWrapper, path string) error {
+		// Check if the base route is nil
+		if baseRouter == nil {
+			return gonethttproute.ErrNilRouter
+		}
+
+		// Set the base route
+		c.RouterWrapper = baseRouter.NewGroup(path)
+
+		// Register the controller routes and groups
+		c.RegisterRoutes()
+		c.RegisterGroups()
+		return nil
+	}
+
+	// RegisterRoutes registers the routes
+	func (c *Controller) RegisterRoutes() {}
+
+	// RegisterGroups registers the groups
+	func (c *Controller) RegisterGroups() {}
 )
--- http/factory/module.go
+++ http/factory/module.go
@@ -7,11 +7,8 @@ import (
 type (
 	// ModuleWrapper is the interface for the route module
 	ModuleWrapper interface {
-		Create(baseRouter gonethttproute.RouterWrapper) error
-		CreateSubmodule(submodule ModuleWrapper) error
-		CreateService() func()
-		CreateController() func(baseRouter gonethttproute.RouterWrapper)
-		CreateValidator() func()
+		Create(baseRouter gonethttproute.RouterWrapper, path string) error
+		CreateSubmodule(submodule ModuleWrapper, path string) error
 	}
 
 	// Module is the struct for the route module
@@ -22,54 +19,28 @@ type (
 	}
 )
 
+// NewModule is a function that creates a new instance of the Module struct
+func NewModule(
+	service ServiceWrapper,
+	validator ValidatorWrapper,
+	controller ControllerWrapper,
+) ModuleWrapper {
+	return &Module{
+		Service:    service,
+		Validator:  validator,
+		Controller: controller,
+	}
+}
+
 // Create is a function that creates a new instance of the Module struct
-func (m *Module) Create(baseRouter gonethttproute.RouterWrapper) error {
-	// Create the service struct
-	m.CreateService()()
-
-	// Check if the service is nil
-	if m.Service == nil {
-		return ErrNilService
-	}
-
-	// Create the validator struct
-	m.CreateValidator()()
-
-	// Check if the validator is nil
-	if m.Validator == nil {
-		return ErrNilValidator
-	}
-
-	// Create the controller struct
-	m.CreateController()(baseRouter)
-
-	// Check if the controller is nil
-	if m.Controller == nil {
-		return ErrNilController
-	}
-
-	// Register the controller routes and groups
-	m.Controller.RegisterRoutes()
-	m.Controller.RegisterGroups()
-	return nil
+func (m *Module) Create(
+	baseRouter gonethttproute.RouterWrapper,
+	path string,
+) error {
+	return m.Controller.Create(baseRouter, path)
 }
 
 // CreateSubmodule is a function that creates a new submodule of the Module struct
-func (m *Module) CreateSubmodule(submodule ModuleWrapper) error {
-	return submodule.Create(m.Controller)
-}
-
-// CreateService is a function that creates a new instance of the Service struct
-func (m *Module) CreateService() func() {
-	return func() {}
-}
-
-// CreateValidator is a function that creates a new instance of the Validator struct
-func (m *Module) CreateValidator() func() {
-	return func() {}
-}
-
-// CreateController is a function that creates a new instance of the Controller struct
-func (m *Module) CreateController() func(baseRouter gonethttproute.RouterWrapper) {
-	return func(baseRouter gonethttproute.RouterWrapper) {}
+func (m *Module) CreateSubmodule(submodule ModuleWrapper, path string) error {
+	return submodule.Create(m.Controller, path)
 }
